c440a2b8371c0425c89741997748f7ebdfee6da6
Durgasuryateja_i <114046334+Suryatejaindigimilli@users.noreply.github.com>
2024-08-27 21:22:51 +0530

    Update WeekPopup.js

--- src/WeekPopup.js
+++ src/WeekPopup.js
@@ -4,12 +4,22 @@ import './WeekPopup.css';
 const WeekPopup = ({ course, week, onClose }) => {
     let content = '';
 
-    if (course === 'Design and Implementation of Human-Computer Interfaces') {
+    if (course === 'Design and Implementation of Human Interface') {
         switch (week) {
             case 'Week 5':
                 content = (
                     <ul>
+                        <p><b>Content for Week 5</b></p>
+                        <li>1. D</li>
+                        <li>2. C</li>
+                        <li>3. B</li>
+                        <li>4. B</li>
+                        <li>5. D</li>
+                        <li>6. A</li>
+                        <li>7. C</li>
+                        <li>8. C</li>
                         {/* Add more items as needed */}
+                        {/* <li>Content for Week 5</li> */}
                     </ul>
                 );
                 break;
@@ -22,8 +32,19 @@ const WeekPopup = ({ course, week, onClose }) => {
             case 'Week 5':
                 content = (
                     <ul>
-                        
                         {/* Add more items as needed */}
+                        {/* <li>Content for Week 5</li> */}
+                        <p><b>Content for Week 5</b></p>
+                        <li>1. C</li>
+                        <li>2. B</li>
+                        <li>3. B</li>
+                        <li>4. A</li>
+                        <li>5. D</li>
+                        <li>6. A</li>
+                        <li>7. D</li>
+                        <li>8. B</li>
+                        <li>9. A</li>
+                        <li>10. D</li>
                     </ul>
                 );
                 break;
@@ -31,13 +52,20 @@ const WeekPopup = ({ course, week, onClose }) => {
                 content = `Content for ${week}...`;
                 break;
         }
-        
     } else if (course === 'Sociology of Development') {
         switch (week) {
             case 'Week 5':
                 content = (
                     <ul>
-                        
+                        <p><b>Content for Week 5</b></p>
+                        <li>1. (c) Ethnocentrism</li>
+                        <li>2. (b) Neo-Marxism</li>
+                        <li>3. (d) All of these</li>
+                        <li>4. (d) All of these</li>
+                        <li>5. (a) Ideological, Empirical, and Methodological or Metatheoretical</li>
+                        <li>6. (c) Modernization</li>
+                        <li>7. (d) Hamza Alavi</li>
+                        <li>8. (d) Raul Prebisch</li>
                         {/* Add more items as needed */}
                     </ul>
                 );
@@ -46,7 +74,35 @@ const WeekPopup = ({ course, week, onClose }) => {
                 content = `Content for ${week}...`;
                 break;
         }
-        // else if(course === ''){}
+    }else if (course === 'Social Innovation in Industry 4.0') {
+        switch (week) {
+            case 'Week 5':
+                content = (
+                    <ul>
+                        <p><b>Content for Week 5</b></p>
+                        <li>1. D</li>
+                        <li>2. D</li>
+                        <li>3. D</li>
+                        <li>4. B</li>
+                        <li>5. B</li>
+                        <li>6. B</li>
+                        <li>7. B</li>
+                        <li>8. B</li>
+                        <li>9. B</li>
+                        <li>10. C</li>
+                        <li>11. B</li>
+                        <li>12. A</li>
+                        <li>13. A</li>
+                        <li>14. C</li>
+                        <li>15. D</li>
+                        {/* Add more items as needed */}
+                    </ul>
+                );
+                break;
+            default:
+                content = `Content for ${week}...`;
+                break;
+        }
     } else {
         content = `Content for ${week}...`;
     }
