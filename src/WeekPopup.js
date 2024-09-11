de568f96f323d5351c3041b4f922d8ac5686b519
Durgasuryateja_i <114046334+Suryatejaindigimilli@users.noreply.github.com>
2024-09-11 21:23:43 +0530

    Update WeekPopup.js

--- src/WeekPopup.js
+++ src/WeekPopup.js
@@ -6,19 +6,19 @@ const WeekPopup = ({ course, week, onClose }) => {
 
     if (course === 'Design and Implementation of Human Interface') {
         switch (week) {
-            case 'Week 6':
+            case 'Week 7':
                 content = (
                     <ul>
                         <p><b>Content for Week 6</b></p>
-                        <li>1. D </li>
-                        <li>2. C </li>
-                        <li>3. B </li>
-                        <li>4. B </li>
-                        <li>5. D </li>
-                        <li>6. A </li>
-                        <li>7. C </li>
-                        <li>8. C </li>
-                        <li>9. C </li>
+                        <li>1. A </li>
+                        <li>2. B </li>
+                        <li>3. C </li>
+                        <li>4. A,B </li>
+                        <li>5. C </li>
+                        <li>6. C </li>
+                        <li>7. B </li>
+                        <li>8. B </li>
+                        <li>9. B </li>
                         <li>10. B</li>
                         {/* Add more items as needed */}
                         {/* <li>Content for Week 6</li> */}
@@ -31,21 +31,21 @@ const WeekPopup = ({ course, week, onClose }) => {
         }
     } else if (course === 'Psychology of Learning') {
         switch (week) {
-            case 'Week 6':
+            case 'Week 7':
                 content = (
                     <ul>
                         {/* Add more items as needed */}
                         {/* <li>Content for Week 6</li> */}
                         <p><b>Content for Week 6</b></p>
                         <li>1. C</li>
-                        <li>2. C</li>
-                        <li>3. A</li>
+                        <li>2. D</li>
+                        <li>3. D</li>
                         <li>4. D</li>
                         <li>5. A</li>
                         <li>6. A</li>
                         <li>7. D</li>
-                        <li>8. D</li>
-                        <li>9. C</li>
+                        <li>8. E</li>
+                        <li>9. A</li>
                         <li>10. B</li>
                     </ul>
                 );
@@ -56,7 +56,7 @@ const WeekPopup = ({ course, week, onClose }) => {
         }
     } else if (course === 'Sociology of Development') {
         switch (week) {
-            case 'Week 6':
+            case 'Week 7':
                 content = (
                     <ul>
                         <p><b>Content for Week 6</b></p>
@@ -78,7 +78,7 @@ const WeekPopup = ({ course, week, onClose }) => {
         }
     }else if (course === 'Social Innovation in Industry 4.0') {
         switch (week) {
-            case 'Week 6':
+            case 'Week 7':
                 content = (
                     <ul>
                         <p><b>Content for Week 6</b></p>
@@ -107,11 +107,30 @@ const WeekPopup = ({ course, week, onClose }) => {
         }
     }else if (course === 'Foundation of R language') {
         switch (week) {
-            case 'Week 6':
+            case 'Week 7':
                 content = (
                     <ul>
                         <p><b>Content for Week 6</b></p>
-                        <p>Need to update the answers</p>
+                        <li>1. A</li>
+                        <li>2. C</li>
+                        <li>3. D</li>
+                        <li>4. B</li>
+                        <li>5. C</li>
+                        <li>6. A</li>
+                        <li>7. B</li>
+                        <li>8. B</li>
+                        <li>9. B</li>
+                        <li>10. C</li>
+                        <li>11.D</li>
+                        <li>12. A</li>
+                        <li>13. B</li>
+                        <li>14. A</li>
+                        <li>15. C</li>
+                        <li>16. B</li>
+                        <li>17. D</li>
+                        <li>18. B</li>
+                        <li>19. D</li>
+                        <li>20. C</li>
                         {/* Add more items as needed */}
                     </ul>
                 );
